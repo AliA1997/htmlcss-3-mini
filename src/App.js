52bcc60060a00318df414cfd5ae8e15e2710574c
AliA1997 <alialhaddadAA@outlook.com>
2018-04-09 13:41:11 -0700

    Commit origin

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import classnames from 'classnames';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isAnimating: false,
+      showDrawer: false,
+    };
+  }
   render() {
     return (
       <div className="app">
         <nav className='main-nav'>
         <div className='logo'>START BOOTSTRAP</div>
         <div className='links'>
-          <div className='phone'>MENU</div>
+          <div className='phone'><button onClick={() => this.setState({showDrawer: !this.state.showDrawer})}>
+          MENU</button>
+          </div>
           <div className='desktop'>ABOUT DOWNLOAD CONTACT</div>
         </div>
         </nav>
         <main>
-          <div className='square animation1 animation2'>I am an L7</div>
+          <button onClick={() => this.setState({isAnimating: !this.state.isAnimating})}>Click to Animate</button>
+          <div className={this.state.isAnimating ? 'square animation1' : 'square'}>I am an L7</div>
         </main>
+        <div className={classnames('drawer', {open: this.state.showDrawer})}>
+          <button onClick={() => this.setState({showDrawer: !this.state.showDrawer})}>
+          Close
+          </button><br/>
+          ABOUT<br />
+          DOWNLOAD<br />
+          CONTACT<br />          
+        </div>
       </div>
     );
   }
