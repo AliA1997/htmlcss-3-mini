99bd11b71893195e4ed217c3c80e820d7063c41a
AliA1997 <alialhaddadAA@outlook.com>
2018-04-09 11:04:45 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <nav className='main-nav'>
+        <div className='logo'>START BOOTSTRAP</div>
+        <div className='links'>MENU</div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
